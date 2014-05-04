d956c33847cefadd3b0e575415524b2a1db922be
HectorHernandezMarques <H.HERNANDEZ@ALUMNOS.UPM.ES>
2014-05-04 19:22:29 +0200

    Update Player.java

--- src/uk/ac/gcu/battleships/Player.java
+++ src/uk/ac/gcu/battleships/Player.java
@@ -1,7 +1,20 @@
 package uk.ac.gcu.battleships;
 
 public class Player  {
-	String name;
+	public String name;
+	public Grid myGrid;
+	
+	public Player(){
+		myGrid = new Grid;
+	}
+	
+	public void createShip(int x, int y, int size, char orientation){
+		Ship S = new Ship;
+		S.setPosition(x,y);
+		S.setShipSize(size);
+		S.setShipOrientation(orientation);
+		myGrid.addShip(S);
+	}
 	
 	public boolean makeGuess(Guess G){
 		return(G.get_X()==2&&G.get_Y()==2);
@@ -9,7 +22,7 @@ public class Player  {
 	
 	//TESTING
 public static void main(String[] args) {
-		Player p1 = new Player();								
+		Player p1 = new Player();	
 		Guess g1 = new Guess((byte)2,(byte)2);					
 		Guess g2 = new Guess((byte)2,(byte)1);					
 		System.out.println(p1.makeGuess(g1));
